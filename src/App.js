78cd6d39492587b42c36857b71c4d4a3f8796933
Anas Shahid <muhammad.shahid@ericsson.com>
2020-04-12 14:30:06 -0400

    Added github API, imported users from the api.
    Fixes: https://github.com/Anasshahidd21/GithubFinder/issues/3
    
    Signed-off-by: Anas Shahid <muhammad.shahid@ericsson.com>

--- src/App.js
+++ src/App.js
@@ -1,27 +1,76 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Users from "./components/users";
+// import user from "./components/user";
+import axios from "axios";
 
-function App() {
-  const user = [
-    {
-      username: "anas",
-      location: "www.facebook.com",
-    },
-    {
-      username: "shahid",
-      location: "www.facebook123.com",
-    },
-    {
-      username: "saad",
-      location: "www.gmail.com",
-    },
-  ];
-  return (
-    <div className="App">
-      <Users users={user} />
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    users: [],
+  };
+
+  getAllUsers = async () => {
+    const allUsers = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.GITHUB_CLIENT_ID}?client_secret=${process.env.GITHUB_CLIENT_SECRET}`
+    );
+    return allUsers.data;
+  };
+
+  async componentDidMount() {
+    const users = await this.getAllUsers();
+    this.setState({ users });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Users users={this.state.users} />
+      </div>
+    );
+  }
 }
 
-export default App;
+// getAllUsers = async () => {
+//   const allUsers = await axios.get(
+//     `https://api.github.com/users?client_id=${process.env.GITHUB_CLIENT_ID}?client_secret=${process.env.GITHUB_CLIENT_SECRET}`
+//   );
+//   console.log(allUsers.data);
+//   return allUsers.data;
+// };
+
+// import React, { Component } from "react";
+
+// class App extends Component() {
+//   constructor() {
+//     super();
+//     this.state = {
+//       users: [],
+//     };
+//   }
+
+//   async componentWillMount() {}
+
+//   render() {
+//     const users = [
+//       {
+//         username: "anas",
+//         location: "www.facebook.com",
+//       },
+//       {
+//         username: "shahid",
+//         location: "www.facebook123.com",
+//       },
+//       {
+//         username: "saad",
+//         location: "www.gmail.com",
+//       },
+//     ];
+//     return (
+//       <div className="App">
+//         <Users users={users} />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
--- src/components/users.js
+++ src/components/users.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import User from "./user";
 
-export class users extends Component {
+export class Users extends Component {
   render() {
+    // return (
+    //   <div>
+    //     <h3>Sexy bois</h3>
+    //   </div>
+    // );
     const users = this.props.users;
+    console.log(users);
     const userDetails = users.map((user) => <User user={user} />);
     return userDetails;
   }
 }
 
-export default users;
+export default Users;
